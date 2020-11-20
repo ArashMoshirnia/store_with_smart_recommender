0353bfd6fe950c2228b2f24d1cd33565eebe7521
Arash <arash_moshirnia@yahoo.com>
2020-11-20 15:43:22 +0330

    Add products admin classes

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from products.models import Product, Category, ProductRating
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'is_enabled')
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ('user', 'product', 'rating')
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(ProductRating, RatingAdmin)
