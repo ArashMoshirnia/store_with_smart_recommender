23a78260dbaa5f3253d61d87daa792bbe0d1efd9
Arash <arash_moshirnia@yahoo.com>
2020-11-20 15:29:59 +0330

    Add id to UserSerializer fields

--- users/api/serializers.py
+++ users/api/serializers.py
@@ -8,7 +8,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('username', 'password', 'email')
+        fields = ('id', 'username', 'password', 'email')
 
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
