5112189d9032d33e489a27b3cc16020e5bfe7b96
Arash <arash_moshirnia@yahoo.com>
2021-03-27 21:28:41 +0430

    Fix bugs in products serializers

--- products/api/serializers.py
+++ products/api/serializers.py
@@ -19,7 +19,10 @@ class ProductSerializer(serializers.ModelSerializer):
         fields = ('id', 'name', 'quantity', 'sold_count', 'description', 'avatar', 'category')
 
     def get_avatar(self, obj):
-        avatar_url = obj.avatar.url
+        try:
+            avatar_url = obj.avatar.url
+        except ValueError:
+            return None
         request = self.context.get('request')
         return request.build_absolute_uri(avatar_url)
 
@@ -29,7 +32,19 @@ class ProductRatingSerializer(serializers.ModelSerializer):
         model = ProductRating
         fields = ('user', 'product', 'rating')
 
+    def get_unique_together_validators(self):
+        return []
+
     def validate_rating(self, value):
         if value < 0 or value > 5:
             raise ValidationError('Rating must be a value between 0 and 5')
         return value
+
+    def create(self, validated_data):
+        instance, created = ProductRating.objects.update_or_create(
+            user=validated_data['user'],
+            product=validated_data['product'],
+            defaults={'rating': validated_data['rating']}
+        )
+
+        return instance
