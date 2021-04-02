25c1b3e9b3c74b323e0948b98d630e864fe0ed11
Arash <arash_moshirnia@yahoo.com>
2021-04-02 16:47:04 +0430

    Add static method to User to generate random usernames

--- users/models.py
+++ users/models.py
@@ -1,5 +1,13 @@
+import random
+import string
+
 from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    pass
+
+    @staticmethod
+    def generate_random_username(length=6):
+        lowercase_letters = string.ascii_lowercase
+        digits = string.digits
+        return ''.join(random.choice(lowercase_letters + digits) for i in range(length))
