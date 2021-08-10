ccfddd3c03963d820054f2f418037ae0b81b10f5
Arash <arash_moshirnia@yahoo.com>
2021-08-10 18:50:34 +0430

    Add PurchaseProduct model

--- products/models.py
+++ products/models.py
@@ -22,6 +22,7 @@ class Product(models.Model):
     category = models.ForeignKey(Category, related_name='products', on_delete=models.PROTECT, null=True)
     quantity = models.PositiveIntegerField(default=0)
     sold_count = models.PositiveIntegerField(default=0)
+    price = models.PositiveIntegerField(default=0)
     description = models.TextField(blank=True)
     avatar = models.ImageField(upload_to='avatars/products', null=True, blank=True)
     is_enabled = models.BooleanField(default=True)
@@ -52,3 +53,14 @@ class ProductRating(models.Model):
 
     def __str__(self):
         return '{} - {}: {}'.format(self.user, self.product, self.rating)
+
+
+class PurchaseProduct(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='product_purchases', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name='purchases', on_delete=models.CASCADE)
+    purchase_count = models.PositiveSmallIntegerField()
+    created_time = models.DateTimeField(auto_now_add=True)
+    modified_time = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return '{} - {}: {}'.format(self.user, self.product, self.created_time)
